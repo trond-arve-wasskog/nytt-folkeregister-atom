692159b3addc358f8585950f60c093dd32013c23
Tomas Jansson <tomas.jansson@gmail.com>
2014-06-03 08:29:06 +0200

    more owin testing

--- dotnetES/src/Folkeregister.Web/OwinAppSetup.cs
+++ dotnetES/src/Folkeregister.Web/OwinAppSetup.cs
@@ -1,9 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Folkeregister.Contracts.Commands;
+using Folkeregister.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Newtonsoft.Json;
+using Owin;
+using Simple.Owin.Static;
 using Simple.Web;
+using Simple.Web.OwinSupport;
 
+[assembly: OwinStartup(typeof(OwinAppSetup))]
 namespace Folkeregister.Web
 {
     public class OwinAppSetup
@@ -14,9 +20,31 @@ namespace Folkeregister.Web
                     typeof(CreatePerson)
                 };
 
-        public static void Setup(Action<Func<Func<IDictionary<string, object>, Task>, Func<IDictionary<string, object>, Task>>> use)
+        //public static void Setup(Action<Func<Func<IDictionary<string, object>, Task>, Func<IDictionary<string, object>, Task>>> use)
+        public void Configuration(IAppBuilder app)
         {
-            use(Application.App);
+
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.Objects
+            };
+            //var staticBuilder = Statics.AddFolderAlias("/Scripts", "/scripts").AddFileAlias("/Static/index.html", "/");
+            //app.Use(staticBuilder.Build());
+
+            //var hubConfiguration = new HubConfiguration();
+            //app.MapSignalR("/realtime", hubConfiguration);
+            app.UseSimpleWeb();
+//app.Run(c => Application.App());
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world.");
+            });
+            //use(staticBuilder.Build());
+
+            //use((hm) => hm);
+
+            //use(Application.App);
         }
     }
 }
