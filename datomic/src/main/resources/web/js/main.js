f613c2a18f316b64d9a05d46a4a843e3de98fed6
eivindw <eivindw@gmail.com>
2014-08-18 21:24:21 +0200

    Skit i react og smør sammen et jquery-gui for demo.

--- datomic/src/main/resources/web/js/main.js
+++ datomic/src/main/resources/web/js/main.js
@@ -1,11 +1,59 @@
-/**
- * @jsx React.DOM
- */
 (function () {
    "use strict";
 
-   React.renderComponent(
-      <h1>Hello World!</h1>,
-      document.getElementById("main")
-   );
+   function loadAllPersons() {
+      $.get("/persons", function (persons) {
+         var $main = $("#main");
+
+         $main.html("<h1>Personer</h1>");
+
+         var $personlist = $("<ul/>");
+
+         _.each(persons, function (person) {
+            var $link = $("<a href='#" + person[":person/ssn"] + "'>" + person[":person/name"] + "</a>");
+            $personlist.append($("<li/>").append($link));
+         });
+
+         $main.append($personlist);
+      });
+   }
+
+   function loadPerson(ssn) {
+      $.get("/persons/" + ssn, function (person) {
+         var $main = $("#main");
+
+         $main.html("<h1>Persondetaljer</h1>");
+
+         var $fieldset = $("<fieldset/>");
+
+         createInput($fieldset, "ssn", ssn, "Fødselsnummer");
+         createInput($fieldset, "name", person[":person/name"], "Navn");
+         createInput($fieldset, "birthplace", person[":person/birthplace"], "Fødested");
+         createInput($fieldset, "sivilstatus", person[":person/sivilstatus"], "Sivilstatus");
+
+         $main
+            .append($("<form class='pure-form pure-form-aligned'/>")
+            .append($fieldset));
+
+         console.log("Loaded", person);
+      });
+   }
+
+   function createInput($parent, id, value, label) {
+      if (value) {
+         var $div = $("<div class='pure-control-group'>");
+
+         var $label = $("<label for='" + id + "'>" + label + "</label>");
+         var $input = $("<input readonly class='pure-input-rounded' id='" + id + "' type='text' placeholder='" + label + "' value='" + value + "'>");
+
+         $div.append($label, $input);
+
+         $parent.append($div);
+      }
+   }
+
+   routie({
+      "": loadAllPersons,
+      ":ssn": loadPerson
+   });
 })();
